654788430a0e34fe9d034edaafa39a5dc9162693
TheAvianLord <angela-yixuan21@hotmail.com>
2017-01-30 20:28:14 -0800

    Working on Trap Scripts and made Lose Scene
    we could probably make the lose screen fancier if you want haha...

---
+++ TheSkeletonKing/Assets/Scripts/Traps.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Traps : MonoBehaviour
+{
+    public string trapName;
+    //change to strings later once player class is done
+    public bool soulRed = true;
+    public bool soulBlue = true;
+    public bool soulGreen = true;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D trap)
+    {
+        if (soulRed && (trapName == "spikes" || trapName == "arrows"))
+            {
+            SceneManager.LoadScene("Lose");
+            }
+    }
+
+}
