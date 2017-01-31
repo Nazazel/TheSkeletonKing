952dbebbd837e767fd2948dddf90310f8d823a14
TheAvianLord <angela-yixuan21@hotmail.com>
2017-01-30 17:16:05 -0800

    Button Manager Script

---
+++ TheSkeletonKing/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	public void newGameButton (string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+	}
+	
+	// Update is called once per frame
+	public void ExitGameButton()
+    {
+        Application.Quit;
+    }
+		
+}
+
