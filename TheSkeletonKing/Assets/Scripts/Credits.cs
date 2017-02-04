621be8c6bfbf6dc79dfd4e0ab9d6cf0c3d9da0e9
TheAvianLord <angela-yixuan21@hotmail.com>
2017-02-03 18:30:52 -0800

    Credits is all good, just needs an image

--- TheSkeletonKing/Assets/Scripts/Credits.cs
+++ TheSkeletonKing/Assets/Scripts/Credits.cs
@@ -30,6 +30,14 @@ public class Credits : MonoBehaviour
         yield return new WaitForSeconds(3.0f);
         theText.text = "Project Leads:\n\nNazely Hartoonian\n\nUlises Perez";
         yield return new WaitForSeconds(3.0f);
+        theText.fontSize = 70;
+        theText.text = "Programmers:\nUlises Perez\nYixuan (Angela) Li\nZican Li\nShiyang Fang\nShuzhan Xie";
+        yield return new WaitForSeconds(3.0f);
+        theText.fontSize = 90;
+        theText.text = "Artist:\n\nClaudia O'Flaherty";
+        yield return new WaitForSeconds(3.0f);
+        theText.text = "All audio downloaded from Freesound.org";
+        yield return new WaitForSeconds(3.0f);
         theText.text = " ";
         yield return new WaitForSeconds(3.0f);
         SceneManager.LoadScene("Menu");
