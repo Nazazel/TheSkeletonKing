514e8cb9aac8d12f76ee9d602020d577c3e58116
zicanl <zicanl@uci.edu>
2017-02-06 20:34:17 -0800

    Tutorial

--- TheSkeletonKing/Assets/Scripts/Tutorial.cs
+++ TheSkeletonKing/Assets/Scripts/Tutorial.cs
@@ -9,10 +9,38 @@ public class Tutorial : MonoBehaviour {
 	void Start () {
 		objectiveNum = 0;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (objectiveNum == 0 && GameObject.FindWithTag("TutorialDoor").GetComponent<TutorialDoor>().obj1Complete)
+		{
+			objectiveNum++;
+		}
+		else if (objectiveNum == 1 && GameObject.FindWithTag("TutorialKey").GetComponent<TutorialKey>().obj2Complete)
+		{
+			objectiveNum++;
+		}
+		else if (objectiveNum == 2 && GameObject.FindWithTag("TutorialDoor").GetComponent<TutorialDoor>().obj3Complete)
+		{
+			objectiveNum++;
+		}
+		else if (objectiveNum == 3 && GameObject.FindWithTag("TutorialSoulStation").GetComponent<TutorialSoulStation>().obj4Complete)
+		{
+			objectiveNum++;
+		}
+		else if (objectiveNum == 4 && GameObject.FindWithTag("TutorialKey").GetComponent<TutorialKey>().obj5Complete)
+		{
+			objectiveNum++;
+		}
+		else if (objectiveNum == 5 && GameObject.FindWithTag("TutorialSoulStation").GetComponent<TutorialSoulStation>().obj6Complete)
+		{
+			objectiveNum++;
+		}
+		else if (objectiveNum == 6 && GameObject.FindWithTag("TutorialKey").GetComponent<TutorialKey>().obj7Complete)
+		{
+			objectiveNum++;
+		}
+		else if (objectiveNum == 7 && GameObject.FindWithTag("TutorialDoor").GetComponent<TutorialDoor>().obj8Complete)
 		{
 			objectiveNum++;
 		}
@@ -20,9 +48,6 @@ public class Tutorial : MonoBehaviour {
 
 
 
-
-
-
 	}
-		
+
 }
