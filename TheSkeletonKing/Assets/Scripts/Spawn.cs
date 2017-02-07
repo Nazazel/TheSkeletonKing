b997cc60b2e5ec826fbcd02b2b5ac68b2600cfca
TheAvianLord <angela-yixuan21@hotmail.com>
2017-02-06 20:50:45 -0800

    Spawn Works

---
+++ TheSkeletonKing/Assets/Scripts/Spawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject player;
+    public Transform[] spawnPoints;
+    public int spawnPointIndex;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Instantiate(player, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
