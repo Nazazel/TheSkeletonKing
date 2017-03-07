862252c2170367eb3702ef850a18b6894b15c3f3
CodiakVII <codiak759@gmail.com>
2017-03-06 19:31:49 -0800

    g2g fast

--- TheSkeletonKing/Assets/Scripts/PlayerController.cs
+++ TheSkeletonKing/Assets/Scripts/PlayerController.cs
@@ -29,7 +29,7 @@ public class PlayerController : MonoBehaviour {
 					rb.velocity = new Vector2 (0.0f, 0.0f);
 				}
 				if (rb.velocity.x > -3.0f) {
-					rb.velocity = new Vector2 (-3.5f, 0.0f);
+					rb.velocity = new Vector2 (-1.0f, 0.0f);
 				}
 			} else if (Input.GetKey (KeyCode.RightArrow) && !Input.GetKey (KeyCode.LeftArrow) && !Input.GetKey (KeyCode.UpArrow) && !Input.GetKey (KeyCode.DownArrow)) {
 				if ((rb.velocity.x < 0.0f) && (rb.velocity.y != 0.0f)) {
@@ -37,7 +37,7 @@ public class PlayerController : MonoBehaviour {
 					
 				}
 				if (rb.velocity.x < 3.0f) {
-					rb.velocity = new Vector2 (3.5f, 0.0f);
+					rb.velocity = new Vector2 (1.0f, 0.0f);
 				}
 			} else if (Input.GetKey (KeyCode.DownArrow) && !Input.GetKey (KeyCode.RightArrow) && !Input.GetKey (KeyCode.UpArrow) && !Input.GetKey (KeyCode.LeftArrow)) {
 				if ((rb.velocity.y > 0.0f) && (rb.velocity.x != 0.0f)) {
@@ -45,14 +45,14 @@ public class PlayerController : MonoBehaviour {
 					
 				}
 				if (rb.velocity.y > -3.0f) {
-					rb.velocity = new Vector2 (0.0f, -3.5f);
+					rb.velocity = new Vector2 (0.0f, -1.0f);
 				}
 			} else if (Input.GetKey (KeyCode.UpArrow) && !Input.GetKey (KeyCode.RightArrow) && !Input.GetKey (KeyCode.LeftArrow) && !Input.GetKey (KeyCode.DownArrow)) {
 				if ((rb.velocity.y < 0.0f) && (rb.velocity.x != 0.0f)) {
 					rb.velocity = new Vector2 (0.0f, 0.0f);
 				}
 				if (rb.velocity.y < 3.0f) {
-					rb.velocity = new Vector2 (0.0f, 3.5f);
+					rb.velocity = new Vector2 (0.0f, 1.0f);
 				}
 			} else {
 				rb.velocity = new Vector2 (0.0f, 0.0f);
