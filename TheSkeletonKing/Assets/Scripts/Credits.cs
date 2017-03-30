302826a53328b6c87d7664de518f59bc27d82df3
TheAvianLord <angela-yixuan21@hotmail.com>
2017-03-30 00:33:09 -0700

    Menu, Credits, Lose Scenes are Good to Go

--- TheSkeletonKing/Assets/Scripts/Credits.cs
+++ TheSkeletonKing/Assets/Scripts/Credits.cs
@@ -28,13 +28,18 @@ public class Credits : MonoBehaviour
     IEnumerator waitThreeSeconds()
     {
         yield return new WaitForSeconds(3.0f);
-        theText.text = "Project Leads:\n\nNazely Hartoonian\n\nUlises Perez";
+        theText.text = "Project Leads:\n\nNazely  Hartoonian\n\nUlises Perez";
         yield return new WaitForSeconds(3.0f);
         theText.fontSize = 50;
         theText.text = "Programmers:\n\nUlises Perez\n\nYixuan (Angela) Li\n\nZican  Li\n\nShiyang Fang\n\nShuzhan Xie";
         yield return new WaitForSeconds(3.0f);
         theText.fontSize = 90;
         theText.text = "Artist:\n\nClaudia O'Flaherty";
+        yield return new WaitForSeconds(3.0f);
+        theText.text = "Audio:\n\nNazely  Hartoonian";
+        yield return new WaitForSeconds(3.0f);
+        theText.fontSize = 50;
+        theText.text = "Design:\n\nUlises Perez\n\nYixuan (Angela) Li\n\n Nazely  Hartoonian";
         yield return new WaitForSeconds(3.0f);
         theText.text = "All audio downloaded from Freesound.org";
         yield return new WaitForSeconds(3.0f);
